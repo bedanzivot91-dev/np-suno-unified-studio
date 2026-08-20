bbc77f5f57c482e8cfe13f8909993a500a901138
bedanzivot91-dev <bedanzivot91@gmail.com>
2026-08-20 06:15:56 +0200

    Add Suno Studio view code-behind

---
+++ overlay/src/NPVideoStudio.App/Views/SunoStudioView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NPVideoStudio.App.Views;
+
+public partial class SunoStudioView : UserControl
+{
+    public SunoStudioView()
+    {
+        InitializeComponent();
+    }
+}
